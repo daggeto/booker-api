e72b78e9da5664a1d4f79625bebee85ae2e1b93c
daggeto <artur@vinted.com>
2016-08-11 22:17:45 +0300

    Fix failing send test

--- spec/interactors/notifications/send_spec.rb
+++ spec/interactors/notifications/send_spec.rb
@@ -2,10 +2,11 @@ describe Notifications::Send do
   describe '.for' do
     let(:receivers) { [create(:user)] }
     let(:params) { { message: 'message'} }
+    let(:response) { double('response', body: {}) }
 
     subject { described_class.for(receivers, params) }
 
-    before { allow_any_instance_of(Net::HTTP).to receive(:request)}
+    before { allow_any_instance_of(Net::HTTP).to receive(:request).and_return(response) }
 
     it 'sends notification' do
       expect_any_instance_of(Net::HTTP).to receive(:request)
