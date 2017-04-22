05137f4553e426c98c6a67320c18ae24ab869303
Artur Ingelevic <arturas@oberlo.com>
2017-04-22 17:14:25 +0300

    Missed semicolumn

--- db/migrate/20170422063012_convert_database_to_utf8mb4.rb
+++ db/migrate/20170422063012_convert_database_to_utf8mb4.rb
@@ -3,7 +3,7 @@ class ConvertDatabaseToUtf8mb4 < ActiveRecord::Migration
     execute('
       ALTER TABLE `events` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
       MODIFY `description` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `status` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      MODIFY `status` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
     ')
   end
 end
