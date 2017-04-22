12408704e8cae3cb8b8c89f71662fc75582f0441
Artur Ingelevic <arturas@oberlo.com>
2017-04-22 17:40:33 +0300

    Set uid for user in factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     first_name { Faker::Name::first_name }
     last_name { Faker::Name::last_name }
     email { Faker::Internet.email }
+    uid { email }
     password { Faker::Lorem.characters(10) }
 
     trait :with_service do
