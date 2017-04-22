a9539a1b98bc3fe07bc0d987acaf50a51453b0ec
Artur Ingelevic <arturas@oberlo.com>
2017-04-22 17:00:31 +0300

    Migrate only users to emoji

--- db/migrate/20170422063012_convert_database_to_utf8mb4.rb
+++ db/migrate/20170422063012_convert_database_to_utf8mb4.rb
@@ -1,36 +1,12 @@
 class ConvertDatabaseToUtf8mb4 < ActiveRecord::Migration
   def change
+    change_column :users, :first_name, :string, limit: 191
+    change_column :users, :last_name, :string, limit: 191
+
     execute('
       ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
       MODIFY `first_name` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
       MODIFY `last_name` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    ')
-
-    execute('
-      ALTER TABLE `support_issues` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `message` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    ')
-
-    execute('
-      ALTER TABLE `services` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `name` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `description` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    ')
-
-    execute('
-      ALTER TABLE `reports` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `message` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    ')
-
-    execute('
-      ALTER TABLE `notifications` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `title` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `message` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    ')
-
-    execute('
-      ALTER TABLE `events` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `description` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
     ')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170422063012) do
   add_index "devices", ["user_id"], name: "index_devices_on_user_id", using: :btree
 
   create_table "events", force: :cascade do |t|
-    t.text     "description", limit: 65535
+    t.string   "description", limit: 255
     t.string   "status",      limit: 255
     t.datetime "start_at"
     t.datetime "end_at"
@@ -54,14 +54,14 @@ ActiveRecord::Schema.define(version: 20170422063012) do
   create_table "notifications", force: :cascade do |t|
     t.string   "uuid",           limit: 255
     t.string   "profile",        limit: 255
-    t.text     "tokens",         limit: 16777215
+    t.text     "tokens",         limit: 65535
     t.integer  "reservation_id", limit: 4
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id",        limit: 4
-    t.string   "title",          limit: 191
-    t.string   "message",        limit: 191
-    t.text     "payload",        limit: 16777215
+    t.string   "title",          limit: 255
+    t.text     "message",        limit: 65535
+    t.text     "payload",        limit: 65535
     t.integer  "sender_id",      limit: 4
     t.string   "sender_type",    limit: 255
   end
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 20170422063012) do
   create_table "reports", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "service_id", limit: 4
-    t.string   "message",    limit: 191
+    t.string   "message",    limit: 255, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 20170422063012) do
   add_index "service_photos", ["slot"], name: "index_service_photos_on_slot", using: :btree
 
   create_table "services", force: :cascade do |t|
-    t.string   "name",        limit: 191
+    t.string   "name",        limit: 255
     t.integer  "duration",    limit: 4,     default: 60,    null: false
     t.integer  "price",       limit: 4,     default: 0,     null: false
     t.datetime "created_at",                                null: false
@@ -149,13 +149,13 @@ ActiveRecord::Schema.define(version: 20170422063012) do
 
   create_table "support_issues", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
-    t.text     "message",        limit: 65535
+    t.text     "message",        limit: 65535, null: false
     t.string   "platform",       limit: 255
     t.string   "version",        limit: 255
     t.string   "app_version",    limit: 255
-    t.text     "device_details", limit: 16777215
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.text     "device_details", limit: 65535
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   add_index "support_issues", ["user_id"], name: "index_support_issues_on_user_id", using: :btree
