164364c4a43041f76ce3de50b405c47d00ec90a5
Artur Ingelevic <arturas@oberlo.com>
2017-04-22 17:08:12 +0300

    Migrate all events columns

--- db/migrate/20170422063012_convert_database_to_utf8mb4.rb
+++ db/migrate/20170422063012_convert_database_to_utf8mb4.rb
@@ -2,7 +2,8 @@ class ConvertDatabaseToUtf8mb4 < ActiveRecord::Migration
   def change
     execute('
       ALTER TABLE `events` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-      MODIFY `description` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+      MODIFY `description` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      MODIFY `status` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     ')
   end
 end
